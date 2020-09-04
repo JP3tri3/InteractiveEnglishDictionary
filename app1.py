8bd79597fc2fe453d53a80e4b0ee14d6cdeb3ef5
Plan3t <plan3t@Plan3ts-iMac-Pro.local>
2020-09-04 18:21:24 -0400

    yn conditional added

--- app1.py
+++ app1.py
@@ -6,21 +6,21 @@ data = json.load(open("data.json"))
 
 word = input("Enter word: ").lower()
 
-# def getMatches(word):
-#     difflib.get_close_matches(word, data.keys())
-
 
 def getDefinition(word):
     if word in data:
         return data[word]
     elif len(difflib.get_close_matches(word, data.keys())) > 0:
         yn = input("Did you mean %s instead?  Enter Y if yes, or N if no." %
-                   get_close_matches(word, data.keys())[0])
-        if yn == "Y":
+                   get_close_matches(word, data.keys())[0]).lower()
+        if yn == "y":
             return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "n":
+            return("The word doesn't exist, Try again")
+        else:
+            return("Y or N not selected, Try again")
     else:
         return("Word not Found, try again.")
 
 
 print(getDefinition(word))
-# print(getMatches(word))
